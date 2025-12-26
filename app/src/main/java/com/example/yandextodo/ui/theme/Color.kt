d69e01259ff7b40bc4866c37545c714979b5c452
Save1ik <Saveliy.kapelzon@yandex.ru>
2025-12-26 16:05:49 +0500

    Initial commit

---
+++ app/src/main/java/com/example/yandextodo/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.example.yandextodo.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val ScarletPrimary = Color(0xFFDC143C)
+val ScarletDark = Color(0xFF8B0000)
+val ScarletLight = Color(0xFFFF6B6B)
